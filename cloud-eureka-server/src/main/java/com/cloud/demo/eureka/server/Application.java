454965ee6075bc8082cb829b50f80b1f21b00d64
cuixiuyin <1099442418@qq.com>
2019-06-23 16:02:41 +0800

    initial commit

---
+++ cloud-eureka-server/src/main/java/com/cloud/demo/eureka/server/Application.java
@@ -0,0 +1,19 @@
+package com.cloud.demo.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author : cuixiuyin
+ * @date : 2019/6/23
+ */
+//启动一个服务注册中心
+@EnableEurekaServer
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
