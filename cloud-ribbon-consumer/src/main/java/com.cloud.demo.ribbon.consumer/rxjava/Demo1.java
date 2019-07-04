12504cf7906bba65284e6e3a6fefe727823cee3b
cuixiuyin <1099442418@qq.com>
2019-07-04 20:55:57 +0800

    RxJava

---
+++ cloud-ribbon-consumer/src/main/java/com.cloud.demo.ribbon.consumer/rxjava/Demo1.java
@@ -0,0 +1,50 @@
+package com.cloud.demo.ribbon.consumer.rxjava;
+
+import rx.Observable;
+import rx.Subscriber;
+
+/**
+ * @author : cuixiuyin
+ * @date : 2019/7/4
+ */
+public class Demo1 {
+
+    // 1.Observable (观察者、事件源)用来向订阅者Subscriber（订阅者）对象发布事件，Subscriber对象则在接收到事件后对其进行处理，而在这里所指的事件通常就是对依赖服务的调用。
+    // 2.一个 Observable 可以发出多个事件，直到结束或是发生异常。
+    // 3.Observable 对象每发出一个事件，就会调用对应订阅者 Subscriber 对象的onNext()方法。
+    // 4.每一个 Observable 的执行，最后一定会通过调用 Subscriber. onCompleted()或者Subscriber.onError()来结束该事件的操作流。
+    public static void main(String[] args) {
+
+        Observable<String> observable = Observable.create(new Observable.OnSubscribe<String>() {
+
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+                subscriber.onNext("Hello RxJava");
+                subscriber.onNext("JMCui");
+                subscriber.onCompleted();
+            }
+        });
+
+        Subscriber<String> subscriber = new Subscriber<String>() {
+
+            @Override
+            public void onCompleted() {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onNext(String s) {
+                System.out.println("Subscriber:" + s);
+            }
+        };
+
+        // 订阅
+        observable.subscribe(subscriber);
+
+    }
+}
